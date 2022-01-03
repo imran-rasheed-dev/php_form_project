a4876f4d86f57e620eb4702dbb28289472f368c1
imran-rasheed-dev <itsimranwebdev@gmail.com>
2022-01-03 12:16:52 +0500

    my first php website

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="continer">
+        <h1>
+            This is my first website in php
+        </h1>
+        <div>
+            <?php
+                echo "hello world";
+             
+            ?>
+
+           <?php
+                $myvar1 = 100;
+                $myvar2 = 200;
+
+                echo $myvar1;
+                echo $myvar2;
+
+                // operators in php
+
+                // 1.Artmathic operators
+                // 2.Assignment operators
+                // 3.Comparison operators
+                // 4.Logical operators
+           ?>
+        </div>
+    </div>
+</body>
+</html>
