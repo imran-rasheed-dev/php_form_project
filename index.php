4b8b55b651ddba1368c0390d58b10e99fd4bb9e9
imran-rasheed-dev <itsimranwebdev@gmail.com>
2022-01-03 16:01:06 +0500

    operetors in php completed

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Php</title>
 </head>
 <body>
     <div class="continer">
@@ -29,23 +29,82 @@
                 // operators in php
                 
                 // 1.Artmathic operators
-                
-                echo "The value of myvar1 + myvar2 is <br>";
+                echo "<h2>Artmathic operators </h2>";
+                echo "The value of myvar1 + myvar2 is";
                 echo $myvar1 + $myvar2;
                 echo "<br>";
-                echo "The value of myvar1 * myvar2 is <br>";
-                echo "<br>";
+                echo "The value of myvar1 * myvar2 is";
                 echo $myvar1 * $myvar2;
                 echo "<br>";
-                echo "The value of myvar1 / myvar2 is <br>";
+                echo "The value of myvar1 / myvar2 is";
+                echo $myvar1 / $myvar2;
                 echo "<br>";
-                echo "The value of myvar1 - myvar2 is <br>";
-                echo "<br>";
+                echo "The value of myvar1 - myvar2 is";
                 echo $myvar1 - $myvar2;
+                echo "<br>";
                 
                 // 2.Assignment operators
+                echo "<h2>Assignment operators </h2>";
+                $newvar = $myvar1;
+                
+                echo "The value of newvar is";
+                echo $newvar;
+                echo "<br>";
+                $newvar += 1;
+                echo "The value of newvar is";
+                echo $newvar;
+                echo "<br>";
+                $newvar -= 1;
+                echo "The value of newvar is";
+                echo $newvar;
+                echo "<br>";
+                $newvar *= 2;
+                echo "The value of newvar is";
+                echo $newvar;
+                echo "<br>";
+                $newvar /= 2;
+                echo "The value of newvar is";
+                echo $newvar;
+                
+                
                 // 3.Comparison operators
-                // 4.Logical operators
+                
+                echo "<h2>comparison operators </h2>";
+                echo "the value of 1==4 is ";
+                echo var_dump(1==4);
+                echo "<br>";
+                echo "the value of 1!=4 is ";
+                echo var_dump(1!=4);
+                echo "<br>";
+                echo "the value of 1>=4 is ";
+                echo var_dump(1>=4);
+                echo "<br>";
+                echo "the value of 1<=4 is ";
+                echo var_dump(1<=4);
+                echo "<br>";
+                
+                
+                // 4.Increament/Decrement operators
+                echo "<h2>Increament/Decrement operators</h2>";
+                $myvar3 = 40;
+                // $myvar3++;
+                // $myvar3--;
+                // ++$myvar3;
+                --$myvar3;
+                echo $myvar3;
+                echo "<br>";
+                echo $myvar3;
+                echo "<br>";
+                
+                
+                // 5.Logical operators
+                echo "<h2>Logical operators</h2>";
+                echo "<div>and (&&),<br>or (||),<br>xor,<br>not (!)</div>";
+                echo "<br>";
+
+                // $mynewvar = (true and true);
+                $mynewvar = (true and false);
+                echo var_dump($mynewvar);
            ?>
         </div>
     </div>
